467e3231d46357d9548bdf92c8a5564fe360e5b7
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-08-04 23:15:26 +0200

    keys const

---
+++ keys.go
@@ -0,0 +1,11 @@
+package mygameengine
+
+const KEY_RIGHT int = 57
+const KEY_LEFT int = 58
+const KEY_UP int = 60
+const KEY_DOWN int = 59
+const KEY_CTRL_RIGHT int = 104
+const KEY_CTRL_LEFT int = 100
+const KEY_SPACE int = 1
+
+/* FIXME */
